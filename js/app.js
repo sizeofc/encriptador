c7a1a266038ff7b33761354bd02d180e13e255ff
Jonatan Atencio <aa.jonatan@gmail.com>
2024-08-02 10:39:58 -0300

    feat: se agrego validaciones visuales con javascript

--- js/app.js
+++ js/app.js
@@ -1,23 +1,21 @@
 
 
 function encriptar() {
-    const inputText = document.getElementById('mensaje').value;
-    if (esValido(inputText)) {
-        if (inputText.trim() !== '') {
-            document.getElementById('no-message').style.display = 'none';
-            document.getElementById('resultado').style.display = 'block';
-            // lógica de encriptación
-            const mapeo = {
-                'e': 'enter',
-                'i': 'imes',
-                'a': 'ai',
-                'o': 'ober',
-                'u': 'ufat'
-            };
-            let encriptado = inputText.split('').map(letra => mapeo[letra] || letra).join('');
-            document.getElementById('output-text').value = encriptado;
-            
-        }
+    let inputText = document.getElementById('mensaje').value;
+    if (inputText.trim() !== '') {
+        document.getElementById('no-message').style.display = 'none';
+        document.getElementById('resultado').style.display = 'flex';
+        // lógica de encriptación
+        const mapeo = {
+            'e': 'enter',
+            'i': 'imes',
+            'a': 'ai',
+            'o': 'ober',
+            'u': 'ufat'
+        };
+        let encriptado = inputText.split('').map(letra => mapeo[letra] || letra).join('');
+        document.getElementById('output-text').value = encriptado;
+        document.getElementById('mensaje').value = "";
     }
 }
 
@@ -25,7 +23,7 @@ function desencriptar() {
     const inputText = document.getElementById('mensaje').value;
     if (inputText.trim() !== '') {
         document.getElementById('no-message').style.display = 'none';
-        document.getElementById('resultado').style.display = 'block';
+        document.getElementById('resultado').style.display = 'flex';
         // lógica de encriptación
         let encriptado = inputText
             .replace(/enter/g, 'e')
@@ -43,17 +41,41 @@ function copiar() {
     const outputText = document.getElementById('output-text');
     outputText.select();
     document.execCommand('copy');
+    showMessage("Texto copiado!");
+    document.getElementById('no-message').style.display = 'flex';
+    document.getElementById('resultado').style.display = 'none';
+    outputText.value="";
 }
 
 function esValido(texto) {
     // Expresión regular para verificar letras minúsculas y espacios
-    const regex = /^[a-z\s]+$/;
+    const regex = /^[A-Zàáâãäåæèéêëìíîïòóôõöøùúûüýÿñçşĉĝĥĵŝŭ]+$/;
     return regex.test(texto);
 }
 
-const textarea = document.getElementById('mensaje');
+function showMessage(text) {
+    let dialogo = document.getElementById('show-message');
+    dialogo.innerHTML = text;
+    dialogo.showModal();
+    //function setTimeout 2s for close dialog
+    setTimeout(function () {
+        dialogo.close();
+    }, 2000);
+}
 
-textarea.addEventListener('input', () => {
-  textarea.style.height = 'auto';
-  textarea.style.height = textarea.scrollHeight + 'px';
-});
+let textArea = document.getElementById('mensaje');
+textArea.addEventListener('input', function () {
+    if (esValido(textArea.value[textArea.value.length - 1])) {
+        textArea.setAttribute("maxlength", textArea.value.length);
+        textArea.classList.add('textarea-error');
+        document.getElementById('boton_encriptar').disabled = true;
+        showMessage("no se permite MAYUSCULAS ni acentos");
+    } else {
+        textArea.removeAttribute("maxlength");
+        textArea.classList.remove('textarea-error');
+        document.getElementById('boton_encriptar').disabled = false;
+
+    }
+
+})
+
